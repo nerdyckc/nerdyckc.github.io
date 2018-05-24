7a8e7fdc3ea999565c4780af950eae89466832cb
nerdyckc <38976151+nerdyckc@users.noreply.github.com>
2018-05-24 12:40:37 +0800

    Add files via upload

---
+++ assets/asdf.js
@@ -0,0 +1,18 @@
+
+//$('section[id="splash"]').on("click", function() {
+  //$('h1').css("visibility","visible");
+  //alert ("testing");
+//});
+
+
+$('section[id="splash_img"]').hover(function() {
+  $('h1').css("visibility","visible");
+  $('h1').addClass("zoomIn animated");
+  //alert ("testing");
+});
+
+
+//$('h1[id="splash"]').hover(function() {
+  //$('h1').css("visibility","visible");
+  //alert ("testing");
+//});
